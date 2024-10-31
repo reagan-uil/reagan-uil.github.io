5cf306504288d8ef15ce5824bf1f63ff66b14533
William Liu <orangetnt827@gmail.com>
2024-10-30 19:46:01 -0500

    working homepage

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import '../app.css';
+	import Navbar from '../lib/components/Navbar.svelte';
+	import { base } from '$app/paths';
+</script>
+
+<svelte:head>
+	<title>William Liu</title>
+</svelte:head>
+<div>
+	<Navbar {base} />
+	<slot></slot>
+</div>
