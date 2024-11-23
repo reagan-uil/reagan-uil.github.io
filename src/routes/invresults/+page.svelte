7ef757a6a1c7417fb444bd58af24603e3a87e868
William Liu <orangetnt827@gmail.com>
2024-11-22 21:18:02 -0600

    inv

--- src/routes/invresults/+page.svelte
+++ src/routes/invresults/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <main>
-  <Header name="Team Results" />
+  <Header name="Invitational Results" />
   <div>
     <div>
       <h1>Roosevelt Invitational Meet</h1>
@@ -29,56 +29,5 @@
         9th and 10th grades
       </li>
     </div>
-    <h1>2024 State Meet</h1>
-    <p>
-      The State meet was held May 14, 2024. Congratulations to Cindy Li on
-      becoming the Biology state champion!
-    </p>
-    <li>
-      Science - Cindy Li - <b>STATE CHAMPION - Biology - </b>24th place overall
-    </li>
   </div>
-  <a
-    href="https://postings.speechwire.com/r-uil-academics.php?conference=6&state=1&seasonid=16"
-    target="_blank">Meet results can be found here.</a
-  >
-  <div>
-    <h1>2024 Regional Meet</h1>
-    <p>Our Regional meet was held on April 26 and 27, 2024.</p>
-    <li>
-      Number Sense - team 6th place, William Liu - 18th place, Johnson Wei -
-      22nd place, Aarnav Shah - 34th place
-    </li>
-    <li>Mathematics - William Liu - 13th place</li>
-    <li>Calculator - William Liu - 25th place</li>
-    <li>
-      Computer Science - team 7th place, William Liu - 17th place, Po Han Ai -
-      28nd place, Yavuz Inan - 33rd place, Jack Mitchell - 38th place
-    </li>
-    <li>
-      Science - team 2nd place, Cindy Li - 2nd place -
-      <span class="bold">State Qualifier!</span>, Alexis Tao - 18th place, Julia
-      Kazunas - 20th place, Yavuz Inan - 32nd place
-    </li>
-    <li>
-      Current Issues and Events - team 3rd place, Cody Cox - 8th place, Hudson
-      Locke - 10th place, Montserrat Arreola - 22nd place, Charles Wentworth -
-      29th place
-    </li>
-    <li>Literary Criticism - Ariana Hernandez - 23rd place</li>
-    <li>Social Studies - Manuel Mora - 17th place</li>
-    <li>
-      Spelling - team 7th place, Paige Opaska - 11th place, Mariana Montemayor -
-      21st place, Nicole Keller - 28th place, Esshal Maknotiya - 34th place
-    </li>
-  </div>
-  <a
-    href="https://postings.speechwire.com/r-uil-academics.php?conference=6&region=4&seasonid=16"
-    target="_blank">Meet results can be found here.</a
-  >
-  <footer>
-    <a href="https://postings.speechwire.com/r-uil.php" target="_blank"
-      >All District, Region, and State results can be found here.</a
-    >
-  </footer>
 </main>
