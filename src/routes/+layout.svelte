ceaed1e1ce0945b5be560cb8aec3a59ba50e6800
William Liu <orangetnt827@gmail.com>
2024-12-05 19:34:23 -0600

    add new favicons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,16 @@
 
 <svelte:head>
   <title>Reagan Academic UIL</title>
-  <link rel="icon" type="image/x-icon" href="/favicon.ico">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48x48.png" />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="192x192"
+    href="/favicon-192x192.png"
+  />
+
   <meta
     name="google-site-verification"
     content="1DNu9e5DGIMB-kTv1OlTyWpkv76NKQ3Lqpp_D_2Y3d8"
