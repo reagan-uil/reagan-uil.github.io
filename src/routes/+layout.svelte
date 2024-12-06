b7d5a125033d6db96a5b334db883e53b710bfa95
William Liu <orangetnt827@gmail.com>
2024-12-05 19:12:51 -0600

    change favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
 
 <svelte:head>
   <title>Reagan Academic UIL</title>
-  <link rel="shortcut icon" type="shortcut icon" href="/favicon.ico" />
+  <link rel="icon" type="image/x-icon" href="/favicon.ico">
   <meta
     name="google-site-verification"
     content="1DNu9e5DGIMB-kTv1OlTyWpkv76NKQ3Lqpp_D_2Y3d8"
