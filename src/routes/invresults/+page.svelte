d6c894e684bf74c3fd078a1839431206bcf857ac
William Liu <orangetnt827@gmail.com>
2024-12-08 10:11:54 -0600

    add some madison results

--- src/routes/invresults/+page.svelte
+++ src/routes/invresults/+page.svelte
@@ -5,6 +5,17 @@
 <Header name="Invitational Results" />
 <div>
   <div>
+    <h1>Madison Invitational Meet</h1>
+    <p>This meet was held on December 7, 2024.</p>
+    <li>Number Sense - Aarnav Shah - 8th place, Levi Doles - 10th place</li>
+    <li>Calculator Applications - Yashwanth Movva - 9th place</li>
+    <li>Mathematics - Aarnav Shah - 3rd place, Levi Doles - 4th place</li>
+    <li>
+      Science - Angela Huang - 8th place 11/12th, Aarnav Shah - 6th place
+      9th/10th
+    </li>
+  </div>
+  <div>
     <h1>Roosevelt Invitational Meet</h1>
     <p>
       The first meet of the season was held on November 2, 2024. The events held
--- src/routes/schedule/+page.svelte
+++ src/routes/schedule/+page.svelte
@@ -3,12 +3,12 @@
   let headers = ['Competition', 'Date', 'Location'];
   let rows = [
     [
-      'TMSCA #3',
+      'Roosevelt Invitational',
       'Saturday 11/2/2024',
       'Roosevelt High School (Math, Number Sense, Calculator, Science only)',
     ],
-    ['TMSCA #6', 'Saturday 12/7/2024', 'Madison High School'],
-    ['TMSCA #8', 'Saturday 1/25/2025', 'Johnson High School'],
+    ['Madison Invitational', 'Saturday 12/7/2024', 'Madison High School'],
+    ['Johnson Invitational', 'Saturday 1/25/2025', 'Johnson High School'],
     ['MacArthur Invitational', 'Saturday 2/8/2025', 'MacArthur High School'],
     ['UIL Districts', '3/28 to 3/29, 2025', 'Reagan High School'],
     ['UIL Regionals', '4/25 to 4/26, 2025', 'UTSA'],
